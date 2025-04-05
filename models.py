11f7b2d6eb4c29c31cae44f5aac94322a6b69c05
Frank <fbassard@wayup.com>
2025-04-05 19:54:55 -0400

    Simplify models

--- models.py
+++ models.py
@@ -1,10 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import json
 
 db = SQLAlchemy()
 
-# Junction tables
 player_campaign = db.Table('player_campaign',
     db.Column('player_id', db.String, db.ForeignKey('players.player_id'), primary_key=True),
     db.Column('campaign_id', db.String, db.ForeignKey('campaigns.campaign_id'), primary_key=True)
@@ -44,7 +41,6 @@ class Player(db.Model):
     clan_id = db.Column(db.String, db.ForeignKey('clans.clan_id'))
     custom_field = db.Column(db.String)
     
-    # Relationships
     devices = db.relationship('Device', secondary=player_device, lazy='subquery',
                              backref=db.backref('players', lazy=True))
     items = db.relationship('Item', secondary=player_item, lazy='subquery',
@@ -52,56 +48,6 @@ class Player(db.Model):
     campaigns = db.relationship('Campaign', secondary=player_campaign, lazy='subquery',
                                backref=db.backref('players', lazy=True))
     clan = db.relationship('Clan', backref='players')
-    
-    def to_dict(self):
-        """Convert player object to dictionary for JSON response"""
-        # Get inventory with quantities
-        inventory_dict = {}
-        for item_association in db.session.query(player_item).filter_by(player_id=self.player_id).all():
-            item = Item.query.get(item_association.item_id)
-            inventory_dict[item.key] = item_association.quantity
-        
-        active_campaigns = [campaign.name for campaign in self.campaigns]
-        
-        clan_dict = None
-        if self.clan:
-            clan_dict = {
-                "id": self.clan.clan_id,
-                "name": self.clan.name
-            }
-        
-        devices_list = []
-        for device in self.devices:
-            devices_list.append({
-                "id": device.device_id,
-                "model": device.model,
-                "carrier": device.carrier,
-                "firmware": device.firmware
-            })
-            
-        return {
-            "player_id": self.player_id,
-            "credential": self.credential,
-            "created": self.created.strftime("%Y-%m-%d %H:%M:%SZ") if self.created else None,
-            "modified": self.modified.strftime("%Y-%m-%d %H:%M:%SZ") if self.modified else None,
-            "last_session": self.last_session.strftime("%Y-%m-%d %H:%M:%SZ") if self.last_session else None,
-            "total_spent": self.total_spent,
-            "total_refund": self.total_refund,
-            "total_transactions": self.total_transactions,
-            "last_purchase": self.last_purchase.strftime("%Y-%m-%d %H:%M:%SZ") if self.last_purchase else None,
-            "active_campaigns": active_campaigns,
-            "devices": devices_list,
-            "level": self.level,
-            "xp": self.xp,
-            "total_playtime": self.total_playtime,
-            "country": self.country,
-            "language": self.language,
-            "birthdate": self.birthdate.strftime("%Y-%m-%d %H:%M:%SZ") if self.birthdate else None,
-            "gender": self.gender,
-            "inventory": inventory_dict,
-            "clan": clan_dict,
-            "_customfield": self.custom_field
-        }
 
 
 class Clan(db.Model):
@@ -140,17 +86,3 @@ class Campaign(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     campaign_id = db.Column(db.String, unique=True, nullable=False)
     name = db.Column(db.String, nullable=False)
-    
-    def get_matchers(self):
-        """
-        This is a mock method since we're not storing matchers in the database.
-        In a real application, this would retrieve the data from the database.
-        """
-        return {}
-    
-    def to_dict(self):
-        """Convert campaign object to dictionary"""
-        return {
-            "id": self.campaign_id,
-            "name": self.name
-        }
